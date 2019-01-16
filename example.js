047eba9177f421855baf529bc5fe20867a8a4f40
Ranjan Shrestha <ranjan@sharingapples.com>
2019-01-16 10:15:36 +0545

    Make script file executable

--- example.js
+++ example.js
@@ -8,5 +8,5 @@ class P {
 benchmark('Example exponentiation', () => 2 ** 8);
 benchmark('Example shift', () => 2 << 8);
 benchmark('Spread operation', () => ({...obj, c: 3 }));
-benchmark('Object.assign', () => Object.assign({}, obj, { c: 3 }));
-benchmark('New Object', () => new P(obj));
+benchmark('Object.assign', () => Object.assign({}, { c: 3 }, obj));
+benchmark('New Object', () => { obj.c = 3; new P(obj); });
